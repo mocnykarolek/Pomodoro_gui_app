4dcce5316377f5b1d29d911ddeb0fd3a24d18adf
Karol Oledzki <karolek.oledzki@gmail.com>
2024-03-07 22:42:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,100 @@
+# import time
+import math
+from tkinter import *
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    timer_text.config(text="Timer")
+    canvas.itemconfig(text_canvas, text="00:00")
+    global current_text
+    current_text = ""
+    checkmarks.config(text=current_text)
+    global reps
+    reps = 0
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+reps = 0
+timer = None
+current_text = ""
+
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:  # long break
+        timer_text.config(text="Long break", fg=RED)
+        count_down(long_break_sec)
+
+    elif reps % 2 == 0:  # short break
+        timer_text.config(text="Short break", fg=PINK)
+        count_down(short_break_sec)
+
+    else:  # work
+        timer_text.config(text="Working", fg=GREEN)
+        count_down(work_sec)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(text_canvas, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        global current_text
+        checkmark = "✔"
+        if reps % 2 == 0:
+            current_text = f"{current_text}{checkmark}"
+            checkmarks.config(text=current_text)
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+# TIMER TEXT
+
+timer_text = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
+timer_text.grid(row=0, column=1)
+
+# tomato
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.grid(row=1, column=1)
+text_canvas = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+# BUTTONS
+start_button = Button(text="Start", command=start_timer)
+start_button.grid(row=3, column=0)
+
+reset_button = Button(text="Reset", command=reset_timer)
+reset_button.grid(row=3, column=2)
+
+# CHECKMARKS
+
+checkmarks = Label(bg=YELLOW, fg=GREEN)
+checkmarks.grid(row=4, column=1)
+
+window.mainloop()
